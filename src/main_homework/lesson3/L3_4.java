a01497dc8000ccca67f1b7e3455011e11e7cc93d
YanaPodskrebalina <yanastealth@gmail.com>
2020-05-29 03:13:49 +0300

    L3_4 more short option

--- src/main_homework/lesson3/L3_4.java
+++ src/main_homework/lesson3/L3_4.java
@@ -5,14 +5,13 @@
 package main_homework.lesson3;
 
 public class L3_4 {
-    public static void main (String[] args){
-        String word1 = "The End", word2="End The", w3="The";
-        String w1=word1.substring(0,3);
-        String w2=word2.substring(0,3);
-        if (w1.equals(w3)){
+    public static void main(String[] args) {
+        String word1 = "The End", word2 = "End The";
+        if (word1.substring(0, 3).equals("The")) {
             System.out.println(" \"word1\" string starts with \"The\" : " + word1);
         }
-        if (w2.equals(w3)){
+        if (word2.substring(0, 3).equals("The")) {
             System.out.println("word1 start with \"The\"" + word2);
+        }
     }
-} }
+}
