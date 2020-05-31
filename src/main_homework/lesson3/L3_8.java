264008b66b7d417350c817dbcc9014fb96e557b9
YanaPodskrebalina <yanastealth@gmail.com>
2020-06-01 02:51:46 +0300

    L3_9

--- src/main_homework/lesson3/L3_8.java
+++ src/main_homework/lesson3/L3_8.java
@@ -1,16 +1,18 @@
-//8. Given a string "Den, John, Will, Kate, Adam, Robin". Print all the names from the following string each from a new line.
+//8. Given a string "Den, John, Will, Kate, Adam, Robin". Print all the names from the following string each
+// from a new line.
 //        Example:
 //        Den
 //        John
 //        Will
 //        Kate
-//        ...
-
 
 package main_homework.lesson3;
 
 public class L3_8 {
     public static void main(String[] args) {
-
+        String Str = new String("Den, John, Will, Kate, Adam, Robin");
+        for (String retval : Str.split(", ")) {
+            System.out.println(retval);
+        }
     }
 }
--- src/main_homework/lesson3/L3_9.java
+++ src/main_homework/lesson3/L3_9.java
@@ -6,6 +6,19 @@ package main_homework.lesson3;
 
 public class L3_9 {
     public static void main(String[] args) {
-
+        String a = "sfgs.", b = "hn.j", c = "fdg5";
+        if (!(a.endsWith("."))) {
+            a = a + ".";
+        }
+        if (!(b.endsWith("."))) {
+            b = b + ".";
+        }
+        if (!(c.endsWith("."))) {
+            c = c + ".";
+        }
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
+
