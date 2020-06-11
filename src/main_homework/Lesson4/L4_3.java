d5327c6c8bc9bdbef6c5294676b4746570a32178
YanaPodskrebalina <yanastealth@gmail.com>
2020-06-11 17:38:32 +0300

    Boxer 4_2

--- src/main_homework/Lesson4/L4_3.java
+++ src/main_homework/Lesson4/L4_3.java
@@ -1,8 +1,23 @@
 /*3. Create 3 different classes to demonstate the relationships "has-a" and "uses-a".
 They can be anything you want. For example: Car - Engine - Wheels*/
 
-
+//Inheritance  Relation
 package main_homework.Lesson4;
 
 public class L4_3 {
+    public int wheels;
+    public String engine = "Huge engine V5";
 }
+class Car extends L4_3 {
+
+    public static void main(String[] args) {
+        String carModel = "Ferrari";
+        L4_3 equipment = new L4_3();
+        equipment.wheels = 4;
+        System.out.println("Car Model:" + carModel);
+        System.out.println("Wheels:" + equipment.wheels);
+        System.out.println("Engine:" + equipment.engine);
+
+    }
+}
+
