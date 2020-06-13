6239ffff5a0a7cdb6115d99adc2d98355f5de429
YanaPodskrebalina <yanastealth@gmail.com>
2020-06-14 02:46:55 +0300

    4_5 bunch of constructors

--- src/main_homework/Lesson4/L4_5.java
+++ src/main_homework/Lesson4/L4_5.java
@@ -9,26 +9,26 @@ public class L4_5 {
     double dou;
     String str1, str2;
 
-   public L4_5(int in) {
-      this.in = in;
-            }
+    public L4_5(int in) {
+        this.in = in;
+    }
 
     public L4_5(int in, double dou, String str1) {
-       this.dou = dou;
-       this.str1 = str1;
+        this.dou = dou;
+        this.str1 = str1;
     }
 
     public L4_5(int in, double dou, String str1, String str2) {
         this(in, dou, str1);
-        this.str2=str2;
-   }
+        this.str2 = str2;
+    }
 
     public static void main(String[] args) {
         L4_5 constr1 = new L4_5(2, 2.33, "WOOOW", "RRR");
-         //  L4_5.aa();
+        //  L4_5.aa();
     }
     //public void aa() {
     //    System.out.println("in:"+in+", dou:"+dou+", str1:"+str1+", str2:"+str2);  -почему этот метод не может быть static?
-    }
+}
 
 
