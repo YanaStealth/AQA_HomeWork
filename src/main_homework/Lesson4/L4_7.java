d3c17141b7acc8ddb8d49e4245561f0c8d9f7b3d
yana.podskrebalina <yanastealth@gmail.com>
2020-06-16 00:59:54 +0300

    4_7 initialization blocks

--- src/main_homework/Lesson4/L4_7.java
+++ src/main_homework/Lesson4/L4_7.java
@@ -3,5 +3,29 @@
 package main_homework.Lesson4;
 
 public class L4_7 {
-    
+
+    {
+        System.out.println("init block 1");
+    } //обычный блок инициализации запускается в конструкторе между super
+
+    static {
+        System.out.println("static init block 1");
+    } //статический блок запускается при первом обращении к классу в 1ю очередь
+
+    L4_7() {
+        //super(); по умолчанию стоит, можно не писать, по умолчанию в родительском к-ре
+        System.out.println("constructor");
+    }
+
+    public static void main(String[] args) {
+        new L4_7();
+    }
+
+    {
+        System.out.println("init block 2");
+    }
+
+    static {
+        System.out.println("static init block 2");
+    }
 }
