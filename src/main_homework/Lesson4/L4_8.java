f9d9bac613383b13a9be08ac6095a0ca270f5a7a
yana.podskrebalina <yanastealth@gmail.com>
2020-06-18 02:10:58 +0300

    4_7 - 4_10

--- src/main_homework/Lesson4/L4_8.java
+++ src/main_homework/Lesson4/L4_8.java
@@ -6,13 +6,21 @@ package main_homework.Lesson4;
 
 public class L4_8 {
 
+    static {
+        System.out.println("static init block 1");
+    } //статический блок запускается при первом обращении к классу в 1ю очередь
+
+    static {
+        System.out.println("static init block 2");
+    }
+
     {
         System.out.println("init block 1");
     } //обычный блок инициализации запускается в конструкторе между super
 
-    static {
-        System.out.println("static init block 1");
-    } //статический блок запускается при первом обращении к классу в 1ю очередь
+    {
+        System.out.println("init block 2");
+    }
 
     L4_8() {
         //super(); по умолчанию стоит, можно не писать, по умолчанию в родительском к-ре
@@ -21,13 +29,5 @@ public class L4_8 {
 
     public static void main(String[] args) {
         new L4_8();
-    }
-
-    {
-        System.out.println("init block 2");
-    }
-
-    static {
-        System.out.println("static init block 2");
     }
 }
