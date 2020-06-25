7dba893308a42d12e0c7cfb867c515be1f04b5ab
yana.podskrebalina <yanastealth@gmail.com>
2020-06-26 01:58:04 +0300

    5.4-5.5

--- src/main_homework/Lesson5/L5_5.java
+++ src/main_homework/Lesson5/L5_5.java
@@ -5,7 +5,20 @@
 // - In the second class create an object of the first class
 // - Try to access the private members
 
+//private access means that the member is available only inside its class
+
 package main_homework.Lesson5;
 
+import new_package.A;
+
 public class L5_5 {
+
+    public static void main(String[] args) {
+        A aaa = new A(122, false, "Yappie");
+        L5_3 ccc = new L5_3(65.111111, "Another Package", false);
+        System.out.println(aaa.p); //private, same new_package package
+        System.out.println(ccc.z);  //private, another main_homework package
+    }
+}
+
 }
