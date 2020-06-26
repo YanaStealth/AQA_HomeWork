ca4041c6307305363412a257618b8bfd0984a690
yana.podskrebalina <yanastealth@gmail.com>
2020-06-27 01:49:26 +0300

    5.7

--- src/main_homework/Lesson5/L5_7.java
+++ src/main_homework/Lesson5/L5_7.java
@@ -1,6 +1,9 @@
 //*What is the difference between package-private access and protected access?
+//                    Class
+// Protected access: 1) inside the same class  2)Same package non-subclass, subclass. (Is not accessable for Global)
+// Package-private access inside the same class only
 
-package main_homework.Lesson5;
+//package main_homework.Lesson5;
 
-public class L5_7 {
-}
+//public class L5_7 {
+//}
