f35acf5e0a4c3f0d5c292e23d85ed379d5710ae2
yana.podskrebalina <yanastealth@gmail.com>
2020-06-29 13:13:12 +0300

    5.7-5.8

--- src/main_homework/Lesson5/L5_11.java
+++ src/main_homework/Lesson5/L5_11.java
@@ -1,4 +1,8 @@
-//* Create a class with two different methods and two fields. One of methods should be static, another - non-static. The same thing should be done with class fields(first - static, second - non-static). These two methods should change the corresponding fields of the class when called. Create another class with main method to test your code.
+//* Create a class with two different methods and two fields.
+// One of methods should be static, another - non-static.
+// The same thing should be done with class fields(first - static, second - non-static).
+// These two methods should change the corresponding fields of the class when called.
+// Create another class with main method to test your code.
 //
 //Example:
 //
--- src/main_homework/Lesson5/L5_8.java
+++ src/main_homework/Lesson5/L5_8.java
@@ -3,4 +3,53 @@
 package main_homework.Lesson5;
 
 public class L5_8 {
+    private int prIn1;
+    private String prS2;
+    private boolean prBo3;
+    private double prDo4;
+    private char prCh5;
+
+    public L5_8() {
+    }
+
+    public int getPrIn1() {
+        return prCh5;
+    }
+
+    public void setPrIn1(int newPrIn1) {
+        prIn1 = newPrIn1;
+    }
+
+    public String getPrS2() {
+        return prS2;
+    }
+
+    public void setPrS2(String newPrS2) {
+        prS2 = newPrS2;
+    }
+
+    public boolean getPrBo3() {
+        return prBo3;
+    }
+
+    public void setPrBo3(boolean newPrBo3) {
+        prBo3 = newPrBo3;
+    }
+
+    public double getPrDo4() {
+        return prDo4;
+    }
+
+    public void setPrDo4(double newprDo4) {
+        prDo4 = newprDo4;
+    }
+
+    public char getPrCh5() {
+        return prCh5;
+    }
+
+    public void setPrCh5(char newPrCh5) {
+        prCh5 = newPrCh5;
+    }
+
 }
