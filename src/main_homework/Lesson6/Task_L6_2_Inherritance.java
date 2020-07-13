98c4d2d287836b52ef4351330620e92c5aed769b
yana.podskrebalina <yanastealth@gmail.com>
2020-07-13 23:37:19 +0300

    6_2 Inherritance

---
+++ src/main_homework/Lesson6/Task_L6_2_Inherritance.java
@@ -0,0 +1,56 @@
+/*2) Correct nine classes: Worker (employee), Clerk (clerk), IT (IT specialist), Programmer (programmer), ProjectManager (project manager),
+CTO (CTO), HR (recruiter), OfficeManager (office manager), Cleaner (cleaner).
+Inherit a programmer, project manager and technical director from an IT specialist.
+Inherit the recruiter, cleaner and office manager from the clerk.
+Inherit the clerk and IT professional from the employee. */
+
+package main_homework.Lesson6;
+public class Task_L6_2_Inherritance {
+    public static void main(String[] args) {
+    }
+
+    public class Worker {
+
+    }
+
+    public class IT extends Worker {
+
+    }
+
+    public class Programmer extends IT {
+
+    }
+
+    public class ProjectManager extends IT {
+
+    }
+
+    public class TechnicalDirector extends IT {
+
+    }
+
+    public class Clerk extends Worker {
+
+    }
+
+    public class HR extends Clerk {
+
+    }
+
+    public class Cleaner extends Clerk {
+
+    }
+
+    public class OfficeManager extends Clerk {
+
+    }
+
+
+    public class CTO {
+
+    }
+
+}
+
+
+
