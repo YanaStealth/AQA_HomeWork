63274d1b415b3f0d1c41f8975c89248fea4278d7
yana.podskrebalina <yanastealth@gmail.com>
2020-07-31 21:54:18 +0300

    8_1 Exceptions

---
+++ src/main_homework/Lesson8_Exceptions/L8_1.java
@@ -0,0 +1,17 @@
+/*
+1. Create method to check: if number < 0 print “Number < 0”, if number > 0 print “Number > 0”
+if number = 0 throw your own exception.
+*/
+
+package main_homework.Lesson8_Exceptions;
+
+public class L8_1 {
+    public static void main(String[] args) {
+        L8_1 compare1=new L8_1();
+        compare1.compareNumbers (0);
+    }
+    public void compareNumbers (int a){
+        if(a<0){System.out.println("Number < 0");}
+        else if (a>0){System.out.println("Number > 0");
+    } else {try {throw new RuntimeException("RE");} catch (RuntimeException e){System.out.println("Exception: Number = 0");}
+}}}
