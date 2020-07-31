3387dd711f6df92cc8f2bc153c94d39e56b33579
yana.podskrebalina <yanastealth@gmail.com>
2020-08-01 00:28:43 +0300

    Lesson 8_3 changes 3

--- src/main_homework/Lesson8_Exceptions/L8_3.java
+++ src/main_homework/Lesson8_Exceptions/L8_3.java
@@ -15,23 +15,19 @@ package main_homework.Lesson8_Exceptions;
 
 public class L8_3 {
 
-
     public static void main(String[] args) {
         int a = args.length; //args.length is always = 0; taken from main(String[] args)
         System.out.println(a);
         try {
             int b = 10 / a;
-        } catch (ArithmeticException c) {
-            System.out.println("Division by zero.");
-        }
-        try {
             int[] c = {1};  // 0 ячейка в массиве с, в ней содержится 0
             System.out.println(c[1]); //ячейки не существует, объявлен массив который содержит одну ячейку
+        } catch (ArithmeticException c) {
+            System.out.println("Division by zero.");
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Out of array");
+            System.out.println("Out of array"); //оно все проверяет, но не бросает, так как на первом эксепшене идет выход из трай блока -это ОК
         }
     }
-
 }
 
 
