dd76d7a550a5f6390e69960d047cf7cab667dec0
yana.podskrebalina <yanastealth@gmail.com>
2020-08-01 00:31:09 +0300

    Lesson 8_5 changes 4

--- src/main_homework/Lesson8_Exceptions/L8_5.java
+++ src/main_homework/Lesson8_Exceptions/L8_5.java
@@ -1,13 +1,10 @@
 /*Throw a NullPointerException from the method iCanThrowException();
 Handle it in the main method.
 public class Main {
-
     public static void main(String[] args) {
         iCanThrowException();
     }
-
     public static void iCanThrowException() {
-
     }
 
 } */
@@ -16,19 +13,19 @@ package main_homework.Lesson8_Exceptions;
 public class L8_5 {
     public static void main(String[] args) {
         int[] data = new int[100];
-        iCanThrowException();
+       try {iCanThrowException();
+       }
+       catch (NullPointerException e) {
+// Catch NullPointerExceptions.
+           System.out.println("Attempt to return String for the null object");
+       }
     }
 
     public static void iCanThrowException() {
-
-        try {
-// Instantiate null object.
+        // Instantiate null object.
             Integer age = null;
 // Attempt to call method on object.
             age.toString();
-        } catch (NullPointerException e) {
-// Catch NullPointerExceptions.
-            System.out.println("Attempt to return String for the null object");
         }
     }
-}
+
