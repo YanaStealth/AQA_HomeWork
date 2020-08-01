87f9db919437dba18d50ec381c6d697f1ebb635e
yana.podskrebalina <yanastealth@gmail.com>
2020-08-02 00:23:37 +0300

    Lesson 8_6 final

--- src/main_homework/Lesson8_Exceptions/ExceptionFiveIsNotAllowed.java
+++ src/main_homework/Lesson8_Exceptions/ExceptionFiveIsNotAllowed.java
@@ -11,23 +11,33 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class L8_6 {
+public class ExceptionFiveIsNotAllowed {
     public static void main(String args[]) throws IncorrectFive {
         double[] simpleArray = new double[5];
         System.out.println("Enter 5 digits to calculate the Sum of them");
         Scanner sc = new Scanner(System.in); //System.in An InputStream which is typically connected to keyboard input of console programs.
-        int i = 0;
+        int i=0;
         try {
-            for (i = 0; i < 5; i++) {
-                simpleArray[i] = sc.nextDouble(); //scan data for input of integer value
+        for(i = 0; i < 5; i++) {
+            simpleArray[i] = sc.nextDouble(); //scan data for input of integer value
+            //if(!(sc.hasNextDouble()==true)) { // возвращает истину если с потока ввода можно считать double
+            // } //throw new InputMismatchException("NFC");
+            if (simpleArray[i] == 5) {
+                throw new IncorrectFive("Number 5 is not accepted");
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Number format exception: you can enter integers only" + e.getStackTrace());
-        } catch (Exception e) {
-            System.out.println("General Exception" + e.getStackTrace());
+            }
+            } catch (InputMismatchException e) {
+                  System.out.println("Number format exception: you can enter integers only"+e.getStackTrace());
+
+                } catch (Exception e) {
+                System.out.println("General Exception"+e.getStackTrace());
         }
 
         double totalSum = Arrays.stream(simpleArray).sum();
         System.out.println("Total sum of the array elements is: " + totalSum);
     }
 }
+
+
+// if (!Character.isDigit(simpleArray[i])) {
+// throw new NumberFormatException("NFE");
---
+++ src/main_homework/Lesson8_Exceptions/IncorrectFive.java
@@ -0,0 +1,8 @@
+package main_homework.Lesson8_Exceptions;
+
+public class IncorrectFive extends Exception{
+
+    public IncorrectFive(String errorMessage){
+        super(errorMessage);
+    }
+}
